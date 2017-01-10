fa9ccfee7d727310e786ea3882a4a6255d965fe7
eugenel <leugenel@gmail.com>
2017-01-10 10:03:48 +0200

    More methods in Chapter 2

--- src/main/Chapt2.scala
+++ src/main/Chapt2.scala
@@ -7,26 +7,37 @@ import scala.collection.immutable.StringOps
   */
 object Chapt2 {
   def main(args: Array[String]): Unit = {
-    println(str_multipl_unicode_noloop("Hello"))
+    println(inpower(8,-1))
   }
 
   //The signum of a number is 1 if the number is positive, –1 if it is negative, and 0 if it is zero. Write a function that computes this value
-  def signum (x : Int) = if (x > 0) 1 else if (x == 0) 0 else -1
+  def signum (x : Int) : Int = if (x > 0) 1 else if (x == 0) 0 else -1
 
-  def countdown = for (i <- 0 to 10) println(10-i)
+  def countdown() : Unit = for (i <- 0 to 10) println(10-i)
 
-  def countdown (n:Int) = for (i <- 0 to n) println(n-i)
+  def countdown (n:Int) : Unit = for (i <- 0 to n) println(n-i)
 
-  def str_multipl_unicode (str: String ) = {
+  def str_multipl_unicode (str: String ) : Long = {
     var mltp: Long = 1
     for (i <- str) mltp *= i.toInt
     mltp
   }
-  def str_multipl_unicode_noloop (str: StringOps ) = {
+  def str_multipl_unicode_noloop (str: StringOps ) : Long = {
     var mltp: Long = 1
     str.foreach(i => mltp *= i.toInt)
     mltp
   }
 
+  def str_multipl_unicode_recurs (str: String ) : Long = {
+    if (str.length == 0) return 1
+    str.head * str_multipl_unicode_recurs(str.tail)
+  }
+
+  def inpower (x: Double, n: Int) : Double = {
+    if (n<0) 1/inpower(x,-n)
+    else if (n%2==0 && n>0) inpower(x, n/2)*inpower(x, n/2)
+    else if (n%2!=0 && n>0) x*inpower(x, n-1)
+    else 1
+  }
 
 }
