580d5ce3b3497074cbecbaaa2ffaa3a6a1a1b8af
eugenel <eugenel@liveperson.com>
2017-01-14 16:00:23 +0200

    Adding swap solution ex 3.3

--- src/main/Chapt2.scala
+++ src/main/Chapt2.scala
@@ -1,13 +1,15 @@
 package main
 
 import scala.collection.immutable.StringOps
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 /**
   * Created by eugenel on 1/8/17.
   */
 object Chapt2 {
   def main(args: Array[String]): Unit = {
-    println(inpower(8,-1))
+    println(swapArr(Array(1,2,3,4,5)).mkString(" "))
   }
 
   //The signum of a number is 1 if the number is positive, –1 if it is negative, and 0 if it is zero. Write a function that computes this value
@@ -40,4 +42,32 @@ object Chapt2 {
     else 1
   }
 
+  def randArr(n:Int) : Array[Int] = {
+    val myArra = Array.fill[Int](n)(Random.nextInt())
+    myArra
+  }
+
+  def swapArr(myArr: Array[Int]) : Array[Int] = {
+    //Taken from
+    //http://stackoverflow.com/questions/10158405/swapping-array-values-with-for-and-yield-scala
+
+    // One solution
+    //val myArrOut = (for( i <- 0 until(myArr.length,2); j <- (i+1).to(i,-1) if j<myArr.length ) yield myArr(j)).toArray
+
+    //Grouped explanation
+    println("Grouped")
+    for (i <- myArr.grouped(2)) println(i.mkString(" "))
+
+    //Reverse explanation
+    println("Reversed")
+    for (i <- myArr.grouped(2)) println(i.reverse.mkString(" "))
+
+    //Steps
+    println("Steps")
+    for {b <- myArr.grouped(2); c <- b.reverse} println("b:" +b+" c: "+c)
+
+    //Most valuable solution
+    val myArrOut = (for {b <- myArr.grouped(2); c <- b.reverse} yield c ).toArray
+    myArrOut
+  }
 }
