8d07b48c291489e4a681e5bf3d535335bba15e0b
eugenel <eugenel@liveperson.com>
2017-01-29 14:13:40 +0200

    Continue to chapter 4

--- src/main/Chapt2.scala
+++ src/main/Chapt2.scala
@@ -9,7 +9,8 @@ import scala.util.Random
   */
 object Chapt2 {
   def main(args: Array[String]): Unit = {
-    println(swapArr(Array(1,2,3,4,5)).mkString(" "))
+    //println(remNegativeButFirst(Array(1,-1,2, -4, 2,-4)).mkString(" "))
+    fileWordCount("/Users/eugenel/Google Drive/Courses/ScalaLearn/1 - Getting Started/1 - Subtitles (text) for Course Introduction (2_44).txt")
   }
 
   //The signum of a number is 1 if the number is positive, –1 if it is negative, and 0 if it is zero. Write a function that computes this value
@@ -47,6 +48,7 @@ object Chapt2 {
     myArra
   }
 
+  //ex 3.3
   def swapArr(myArr: Array[Int]) : Array[Int] = {
     //Taken from
     //http://stackoverflow.com/questions/10158405/swapping-array-values-with-for-and-yield-scala
@@ -70,4 +72,58 @@ object Chapt2 {
     val myArrOut = (for {b <- myArr.grouped(2); c <- b.reverse} yield c ).toArray
     myArrOut
   }
+
+  //ex 3.4
+  def positiveFirst(myArr: Array[Int]):Array[Int]={
+    //My solution
+//    val myArrOut = new Array[Int](myArr.length)
+//    val positiveIndxs = for (i <- myArr.indices if myArr(i)>0) yield i
+//    val negatIndxs = for (i <- myArr.indices if myArr(i)<=0) yield i
+//
+//    for (i<-positiveIndxs.indices) myArrOut(i)=myArr(positiveIndxs(i))
+//    for (i<-positiveIndxs.length until positiveIndxs.length+negatIndxs.length)
+//          myArrOut(i)=myArr(negatIndxs(i-positiveIndxs.length))
+//    myArrOut
+
+    //Most valuable solution
+    //http://stackoverflow.com/questions/32109281/produce-new-array-where-all-positive-comes-first-then-negative-or-zero-but-in-s
+    myArr.filter(_ > 0) ++ myArr.filterNot(_ > 0)
+  }
+
+  //3.5
+  def avgArray(myArr: Array[Double]): Double = {
+    myArr.sum/myArr.length
+  }
+
+  //3.7
+  def rmDuplicates (myArr: Array[Int]):Array[Int]={
+    myArr.distinct
+  }
+
+  //3.9
+  //you are given an array buffer of integers and want to remove all but the first negative number
+  def remNegativeButFirst(myArr: Array[Int]):Array[Int]={
+
+    //this is safe in case no negative numbers in the array
+    val firstNeg = myArr.find(_ < 0) match {
+      case Some(x:Int) => x
+      case _  => myArr.length
+    }
+    val twoParts = myArr.splitAt(myArr.indexOf(firstNeg))
+    (twoParts._1 :+ twoParts._2(0)) ++ twoParts._2.filter(_>0)
+  }
+
+  //4.2
+  def fileWordCount (FileName : String) :Unit = {
+
+    val wordsMap = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
+
+    val in = new java.util.Scanner(new java.io.File(FileName))
+
+    while (in.hasNext()) wordsMap(in.next())+=1
+
+    for ( (k,v) <- wordsMap) println("Word:" + k + " "+v+" times")
+
+  }
+
 }
