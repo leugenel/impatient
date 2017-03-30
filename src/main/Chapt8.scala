eddf8f51acfa00c19372a5cf2373ee571b51775c
eugenel <eugenel@liveperson.com>
2017-03-30 18:38:52 +0300

    Continue to chapter 8 exc 1-4

--- src/main/Chapt8.scala
+++ src/main/Chapt8.scala
@@ -7,15 +7,83 @@ package impatient
 
 class BankAccount(initialBalance: Double){
     private var balance = initialBalance
-    def currentBalance = balance
-    def deposit(amount: Double) = {balance += amount; balance}
-    def withdraw(amount: Double) = {balance -= amount; balance}
+    def currentBalance: Double = balance
+    def deposit(amount: Double):Double = {balance += amount; balance}
+    def withdraw(amount: Double):Double = {balance -= amount; balance}
 }
 
+//8.1
 //“Extend the following BankAccount class to a CheckingAccount
 // class that charges $1 for every deposit and withdrawal.”
 class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance) {
     private val comission = 1.0
-    override def deposit(amount: Double) =  super.deposit(amount-comission)
-    override def withdraw(amount: Double) = super.withdraw(amount+comission)
+    override def deposit(amount: Double):Double =  super.deposit(amount-comission)
+    override def withdraw(amount: Double):Double = super.withdraw(amount+comission)
+}
+
+//8.2
+//“Extend the BankAccount class of the preceding exercise into a class SavingsAccount that earns interest every month
+// (when a method earnMonthlyInterest is called) and has three free deposits or withdrawals every month.
+// Reset the transaction count in the earnMonthlyInterest method.”
+class SavingsAccount (initialBalance: Double, interest: Double) extends BankAccount(initialBalance) {
+    private val freeCount = 3
+    private val comission = 1.0
+    private var transactionCount = 0
+
+    override def deposit(amount: Double):Double =  {
+        transactionCount+=1
+        if(transactionCount<=freeCount)  super.deposit(amount)
+        else super.deposit(amount-comission)
+    }
+
+    override def withdraw(amount: Double):Double = {
+        transactionCount+=1
+        if(transactionCount<=freeCount) super.withdraw(amount)
+        else super.withdraw(amount+comission)
+    }
+
+    def earnMonthlyInterest:Double = {
+        transactionCount = 0
+        super.deposit(currentBalance*interest)
+    }
+}
+
+//8.4
+//“Define an abstract class Item with methods price and description.
+// A SimpleItem is an item whose price and description are specified in the constructor.
+// Take advantage of the fact that a val can override a def. A Bundle is an item that contains other items.
+// Its price is the sum of the prices in the bundle.
+// Also provide a mechanism for adding items to the bundle and a suitable description method.”
+
+abstract class Item{
+    def price: Double
+    def description: String
+}
+
+class SimpleItem(override val price: Double, override val description: String) extends Item
+
+class Bundle (items: Array[Item]) extends Item {
+    private var bundle: Array[Item] = items
+
+    override def price:Double = {
+        var bundlePrice: Double = 0
+        for (item <- bundle){
+            bundlePrice+= item.price
+        }
+        bundlePrice
+    }
+
+    override def description: String = {
+        var outputString:String = "The bundle contains: \n"
+        for (item <- bundle){
+            outputString+= item.description + "\n"
+        }
+        outputString+="The bundle price is: "+price.toString
+        outputString
+    }
+
+    def addItemtoBundle(item: SimpleItem) :Unit = {
+        bundle=bundle:+item
+    }
+
 }
--- src/test/scala/Chapt8Test.scala
+++ src/test/scala/Chapt8Test.scala
@@ -7,11 +7,66 @@ import org.scalatest.FunSuite
   * Created by eugenel on 3/29/17.
   */
 class Chapt8Test extends FunSuite{
-  test("Check deposit and withdraw with commissions") {
+  test("CheckingAccount class :Check deposit and withdraw with commissions") {
     var cha:CheckingAccount = new CheckingAccount(0)
     cha.deposit(10)
     assert(cha.currentBalance==9.0)
     cha.withdraw(5)
     assert(cha.currentBalance==3.0)
   }
+
+  test ("SavingsAccount class"){
+    var cha:SavingsAccount = new SavingsAccount(0, 0.1)
+    //#1
+    cha.deposit(10)
+    assert(cha.currentBalance==10)
+    //#2
+    cha.withdraw(5)
+    assert(cha.currentBalance==5)
+    //#3
+    cha.deposit(20)
+    assert(cha.currentBalance==25)
+    //#4 Now they take comission
+    cha.deposit(5)
+    assert(cha.currentBalance==29)
+    //#5
+    cha.deposit(6)
+    //Now they take comission again
+    assert(cha.currentBalance==34)
+    //Interest
+    cha.earnMonthlyInterest
+    assert(cha.currentBalance==37.4)
+    //#1
+    cha.withdraw(7.4)
+    assert(cha.currentBalance==30)
+    //#2
+    cha.withdraw(1)
+    assert(cha.currentBalance==29)
+    //#3
+    cha.withdraw(1)
+    assert(cha.currentBalance==28)
+    //#4
+    cha.withdraw(1)
+    assert(cha.currentBalance==26)
+    //Interest
+    cha.earnMonthlyInterest
+    assert(cha.currentBalance==28.6)
+
+  }
+
+  test ("Class Bundle"){
+    val item1 = new SimpleItem(10, "SimpleItem #1")
+    val item2 = new SimpleItem(20, "SimpleItem #2")
+    assert(item1.description=="SimpleItem #1")
+    assert(item2.description=="SimpleItem #2")
+    assert(item1.price==10)
+    assert(item2.price==20)
+    var bundle = new Bundle(Array(item1, item2))
+    assert(bundle.price==30)
+    val item3= new SimpleItem(30, "SimpleItem #3")
+    bundle.addItemtoBundle(item3)
+    assert(bundle.price==60)
+    println(bundle.description)
+  }
+
 }
