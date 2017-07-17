b3ce3bb6d316c95e1900c8f182d1092fe623a2cf
eugenel <eugenel@liveperson.com>
2017-07-17 09:30:55 +0300

    Chapt 13  finished

--- src/main/Chapt13.scala
+++ src/main/Chapt13.scala
@@ -20,17 +20,55 @@ class Chapt13 {
     }
   }
 
-
   //#3
   //Write a function that removes every second element from a ListBuffer. Try it two ways.
   // Call remove(i) for all even i starting at the end of the list.
   // Copy every second element to a new list. Compare the performance.
 
-
    def rmFromList(mylb: ListBuffer[Int]) : ListBuffer[Int] = {
-     mylb.indices.foldRight(){(a,b)=>{ if(a%2!=0){mylb.remove(a)} } }
+     //mylb.indices.foldRight(){(a,b)=>{ if(a%2!=0){mylb.remove(a)} } }//took 3 ms
+     mylb.indices.reverse.foreach{(i:Int)=> if( i%2!=0 && i<mylb.length ) mylb.remove(i)} //took 2ms
      mylb
+   } //took 2 ms
+  def rmFromList2Other(mylb: ListBuffer[Int]) : ListBuffer[Int] = {
+    var mylbnew = ListBuffer[Int]()
+    mylb.indices.foreach{(i:Int)=> if( i%2==0 && i<mylb.length) mylbnew+=mylb(i)}
+    mylbnew
+  }//took 1 ms
+
+  //#4
+  // Write a function that receives a collection of strings and a map from strings to integers.
+  // Return a collection of integers that are values of the map corresponding to one of the strings in the collection.
+  // For example, given Array("Tom", "Fred", "Harry") and Map("Tom" -> 3, "Dick" -> 4, "Harry" -> 5), return Array(3, 5).
+  // Hint: Use flatMap to combine the Option values returned by get.
+
+   def howProperNames(myarray:Array[String], mymap: Map[String, Int]) = {
+     mymap.flatMap(s =>if(myarray contains s._1){s._2.toString;}
+                  else "" )
    }
+
+  //#5
+  //Implement a function that works just like mkString, using reduceLeft.
+  def likemkString(l: List[String] , sep: String):String = l.reduceLeft{(a,b) => a+sep+b}
+
+  //#7
+  //In SeDction 13.10, “Zipping,” on page 187, the expression (prices zip quantities) map { p => p._1 * p._2 }
+  // is a bit inelegant. We can’t do (prices zip quantities) map { _ * _ } because _ * _ is a function with two arguments,
+  // and we need a function with one argument that is a tuple.
+  // The tupled method of the Function object changes a function with two arguments to one that takes a tuple.
+  // Apply tupled to the multiplication function so you can map it over the list of pairs.
+  def zipLists(l1: List[Double], l2: List[Int]) :List[Double] = {
+    val multiply = ((a:Double,b:Int ) => a * b).tupled
+    (l1 zip l2) map(multiply (_))
+  }
+
+  //#8
+  //Write a function that turns an array of Double values into a two-dimensional array.
+  // Pass the number of columns as a parameter. For example, with Array(1, 2, 3, 4, 5, 6) and three columns,
+  // return Array(Array(1, 2, 3), Array(4, 5, 6)). Use the grouped method.
+
+   def twoDimArray (arr:Array[Double], cnumber:Int) = arr.grouped(cnumber).toArray
+
 
 }
 
--- src/test/scala/Chapt13Test.scala
+++ src/test/scala/Chapt13Test.scala
@@ -27,4 +27,41 @@ class Chapt13Test extends FunSuite {
     assert(new Chapt13().rmFromList(ListBuffer()) == ListBuffer())
   }
 
+  test ("Remove elements 2 other list"){
+    assert(new Chapt13().rmFromList2Other(ListBuffer(0,1,2,3,4,5,6,7,8,9,10,11)) == ListBuffer(0, 2, 4, 6, 8, 10))
+    assert(new Chapt13().rmFromList2Other(ListBuffer(0,1,2,3,4,5,6,7,8,9,10)) == ListBuffer(0, 2, 4, 6, 8, 10))
+    assert(new Chapt13().rmFromList2Other(ListBuffer(0,1)) == ListBuffer(0))
+    assert(new Chapt13().rmFromList2Other(ListBuffer(0)) == ListBuffer(0))
+    assert(new Chapt13().rmFromList2Other(ListBuffer()) == ListBuffer())
+  }
+
+  test ("Proper Array") {
+    assert(new Chapt13().howProperNames(Array("Tom", "Fred", "Harry"), Map("Tom" -> 3, "Dick" -> 4, "Harry" -> 5)) == List('3', '5'))
+    assert(new Chapt13().howProperNames(Array("ATom", "AFred", "AHarry"), Map("Tom" -> 3, "Dick" -> 4, "Harry" -> 5)) == List())
+    assert(new Chapt13().howProperNames(Array("Tom", "Fred", "Harry"), Map("Tom" -> 3, "Fred" -> 4, "Harry" -> 5)) == List('3', '4','5'))
+    assert(new Chapt13().howProperNames(Array(), Map("Tom" -> 3, "Fred" -> 4, "Harry" -> 5)) == List())
+    assert(new Chapt13().howProperNames(Array("Tom", "Fred", "Harry"), Map()) == List())
+  }
+
+  test("mkstring"){
+    assert(new Chapt13().likemkString(List("a", "b", "c"), ":") == "a:b:c")
+    assert(new Chapt13().likemkString(List("a", "b", "c"), "") == "abc")
+    assert(new Chapt13().likemkString(List("a"), ":") == "a")
+  }
+
+  test("zip lists"){
+    assert(new Chapt13().zipLists(List(33.0, 2.0, 7.0), List(3, 4, 5)) == List(99.0, 8.0, 35.0))
+    assert(new Chapt13().zipLists(List(33.0, 2.0, 7.0), List(3)) == List(99.0))
+    assert(new Chapt13().zipLists(List(33.0, 2.0, 7.0), List()) == List())
+    assert(new Chapt13().zipLists(List(), List(3)) == List())
+    assert(new Chapt13().zipLists(List(), List()) == List())
+  }
+
+   test ("two dimension array"){
+     assert(  runtime.ScalaRunTime.stringOf(new Chapt13().twoDimArray(Array(33.0, 2.0, 7.0, 4.0, 1.0, 9.0), 3)) ==  "Array(Array(33.0, 2.0, 7.0), Array(4.0, 1.0, 9.0))"  )
+     assert(  runtime.ScalaRunTime.stringOf(new Chapt13().twoDimArray(Array(33.0, 2.0, 7.0), 1)) ==  "Array(Array(33.0), Array(2.0), Array(7.0))" )
+     assert(  runtime.ScalaRunTime.stringOf(new Chapt13().twoDimArray(Array(33.0, 2.0, 7.0, 4.0, 1.0, 9.0, 1.0, 44.0), 4)) ==  "Array(Array(33.0, 2.0, 7.0, 4.0), Array(1.0, 9.0, 1.0, 44.0))" )
+     assert(  runtime.ScalaRunTime.stringOf(new Chapt13().twoDimArray(Array(33.0), 1)) ==  "Array(Array(33.0))" )
+   }
+
 }
